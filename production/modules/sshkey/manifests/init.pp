f2cc6dbc8d207bf0afe8db3fe31693eed1c486c2
scanaki <mail@scanaki.com>
2018-04-22 23:42:17 +0200

    fix test 2 to init script sshkey

--- production/modules/sshkey/manifests/init.pp
+++ production/modules/sshkey/manifests/init.pp
@@ -11,14 +11,14 @@ class sshkey {
  file { 'id_rsa' :
  	ensure => present,
 	path => '/root/.ssh/id_rsa',
-	source => file("id_rsa"),
+	source => 'puppet:///modules/sshkey/id_rsa',
 	require => File['ssh'],
  }
 
  file { 'id_rsa.pub' :
  	ensure => present,
 	path => '/root/.ssh/id_rsa.pub',
-	source => file("id_rsa.pub"),
+	source => 'puppet:///modules/sshkey/id_rsa.pub',
 	require => File['ssh'],
  }
 
