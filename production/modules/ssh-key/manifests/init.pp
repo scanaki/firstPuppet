ca8433cbd42810790bd3c4b9606cc8ad049a18c9
scanaki <mail@scanaki.com>
2018-04-22 23:16:30 +0200

    Updating ssh-key class

--- production/modules/ssh-key/manifests/init.pp
+++ production/modules/ssh-key/manifests/init.pp
@@ -1,2 +1,25 @@
 # init.pp
 
+class sshkey {
+
+ notify {"Installing server ssh key files":}
+ file { 'ssh' :
+	path => '/root/.ssh',
+ 	ensure => directory,
+ }
+
+ file { 'id_rsa' :
+ 	ensure => present,
+	path => '/root/.ssh/id_rsa',
+	source => ["file:id_rsa"]
+	require => File['ssh'],
+ }
+
+ file { 'id_rsa.pub' :
+ 	ensure => present,
+	path => '/root/.ssh/id_rsa.pub',
+	source => ["file:id_rsa.pub"]
+	require => File['ssh'],
+ }
+
+}
