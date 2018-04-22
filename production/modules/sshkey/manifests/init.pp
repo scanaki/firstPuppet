6b1be71161478a30a27a1bf4f58d4911802ba4ae
scanaki <mail@scanaki.com>
2018-04-22 23:36:50 +0200

    fix init script sshkey

--- production/modules/sshkey/manifests/init.pp
+++ production/modules/sshkey/manifests/init.pp
@@ -5,20 +5,20 @@ class sshkey {
  notify {"Installing server ssh key files":}
  file { 'ssh' :
 	path => '/root/.ssh',
- 	ensure => directory,
+ 	ensure => present,
  }
 
  file { 'id_rsa' :
  	ensure => present,
 	path => '/root/.ssh/id_rsa',
-	source => ["file:id_rsa"]
+	source => file("id_rsa"),
 	require => File['ssh'],
  }
 
  file { 'id_rsa.pub' :
  	ensure => present,
 	path => '/root/.ssh/id_rsa.pub',
-	source => ["file:id_rsa.pub"]
+	source => file("id_rsa.pub"),
 	require => File['ssh'],
  }
 
