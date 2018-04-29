011d263ef42a4433eb83fc890e57ef749105a9db
scanaki <mail@scanaki.com>
2018-04-29 14:39:10 +0200

    fix cron service

--- production/modules/cronagent/manifests/init.pp
+++ production/modules/cronagent/manifests/init.pp
@@ -1,18 +1,15 @@
 class cronagent {
  package { 'cron' :
-  notify {"Installing cron":}
   ensure => installed,
   require => Exec['apt-install'],
  }
 
  service { 'cron' :
-  notify {"Starting cron service":}
   ensure => running,
   enable => true,
  }
 
  cron { 'agent-update' :
-  notify {"Setting cron default jobs":}
   command => '/opt/puppetlabs/puppet/bin/puppet agent --test',
   user => 'root',
   minute => '*/2',
