654d742a97d5271ca16712b632f3390497aa83da
scanaki <mail@scanaki.com>
2018-04-29 14:42:10 +0200

    add notify to cron class

--- production/modules/cronagent/manifests/init.pp
+++ production/modules/cronagent/manifests/init.pp
@@ -1,14 +1,17 @@
 class cronagent {
+ notify {"Installing cron":}
  package { 'cron' :
   ensure => installed,
   require => Exec['apt-install'],
  }
 
+ notify {"Starting cron service":}
  service { 'cron' :
   ensure => running,
   enable => true,
  }
 
+ notify {"Setting cron default jobs":}
  cron { 'agent-update' :
   command => '/opt/puppetlabs/puppet/bin/puppet agent --test',
   user => 'root',
