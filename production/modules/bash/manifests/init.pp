c58ea46b7e36e2009cf5c2e6702a6cd486761c28
scanaki <mail@scanaki.com>
2018-04-29 20:38:46 +0200

    trying somthing else

--- production/modules/bash/manifests/init.pp
+++ production/modules/bash/manifests/init.pp
@@ -6,7 +6,8 @@ class bash {
  file { 'bashrc' :
 	ensure => present,
 	path => '/root/.bashrc',
-	content => template("${module_name}/${fqdn}/bashrc"),
+	#content => template("${module_name}/${fqdn}/bashrc"),
+	source => "${module_name}/${fqdn}/bashrc",
 	require => Package['bash'],
  }
 
