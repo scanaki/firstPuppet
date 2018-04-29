c94b99eb94d0032957127657b28a426760325bb4
scanaki <mail@scanaki.com>
2018-04-29 20:10:54 +0200

    trying another way to get different bashrc files

--- production/modules/bash/manifests/init.pp
+++ production/modules/bash/manifests/init.pp
@@ -6,7 +6,8 @@ class bash {
  file { 'bashrc' :
 	ensure => present,
 	path => '/root/.bashrc',
-	content => template("${module_name}/${fqdn}/bashrc"),
+	#content => template("${module_name}/${fqdn}/bashrc"),
+	source => "puppet:///modules/${module_name}/${fqdn}/bashrc",
 	require => Package['bash'],
  }
 
