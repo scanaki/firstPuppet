7f1931d1e6a61539323060af070ad55cbb66a67f
scanaki <mail@scanaki.com>
2018-05-17 01:15:36 +0200

    simplify class

--- production/modules/sshd/manifests/init.pp
+++ production/modules/sshd/manifests/init.pp
@@ -7,11 +7,7 @@ class sshd {
 	
 	package { 'openssh-server' : 
 		ensure	=> present,
-		before	=> [
-			File['/etc/ssh/sshd_config'],
-			Service['sshd'],
-		]
-	}
+	} ->
 
 ##	Ci-dessous une ecriture identique
 
@@ -19,18 +15,10 @@ class sshd {
 		ensure	  => file,
 		mode		  => '0600',
 		source	  => 'puppet:///modules/sshd/sshd_config',
-		require	  => Package['openssh-server'],
-		before	  => Service['sshd'],
-		notify	  => Service['sshd'],
-	}
+	} ~>
 
 	service { 'sshd' :
 		ensure 	    => running,
 		enable	    => true,
-		subscribe	 => File['/etc/ssh/sshd_config'],
-		require		 => [
-			Package['openssh-server'],
-			File['/etc/ssh/sshd_config'],
-		],
 	}
 }
