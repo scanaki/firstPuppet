1c066f70e90e72292a4a7edced0016f6769d5cc0
scanaki <mail@scanaki.com>
2018-05-17 01:06:50 +0200

    update sshd class

--- production/modules/sshd/manifests/init.pp
+++ production/modules/sshd/manifests/init.pp
@@ -7,7 +7,30 @@ class sshd {
 	
 	package { 'openssh-server' : 
 		ensure	=> present,
-		before	=> File['/etc/ssh/sshd_config'],
+		before	=> [
+			File['/etc/ssh/sshd_config'],
+			Service['sshd'],
+		]
 	}
 
+##	Ci-dessous une ecriture identique
+
+	file { '/etc/ssh/sshd_config' :
+		ensure	  => file,
+		mode		  => '0600',
+		source	  => 'puppet:///modules/sshd/sshd_config',
+		require	  => Package['openssh-server'],
+		before	  => Service['sshd'],
+		notify	  => Service['sshd'],
+	}
+
+	service { 'sshd' :
+		ensure 	    => running,
+		enable	    => true,
+		subscribe	 => File['/etc/ssh/sshd_config'],
+		require		 => [
+			Package['openssh-server'],
+			File['/etc/ssh/sshd_config'],
+		],
+	}
 }
