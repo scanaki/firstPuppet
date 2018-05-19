7bfe06566287d4371d56e46b2ae9960654838897
scanaki <mail@scanaki.com>
2018-05-19 22:45:57 +0200

    add notify to java runtime class

--- production/modules/javaruntime/manifests/jre-9.pp
+++ production/modules/javaruntime/manifests/jre-9.pp
@@ -4,6 +4,7 @@
 # @desc:		Install Java runtime
 
 class javaruntime {
+	notify { "Installing Java runtime environment v.9" :}
 	package { 'openjdk-9-jre' :
 		ensure	 => installed,
 		require	 => Exec['apt-install'],
