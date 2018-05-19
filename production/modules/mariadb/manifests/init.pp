ee0b3685b2d67b3cf26f58bdc0a9c8a37e6a4c57
scanaki <mail@scanaki.com>
2018-05-19 16:27:44 +0200

    Add mysql java connector

--- production/modules/mariadb/manifests/init.pp
+++ production/modules/mariadb/manifests/init.pp
@@ -8,6 +8,11 @@ class mariadb {
 		require => Exec['apt-install'],
 	}
 
+	package { 'libmysql-java' :
+		ensure  => 'installed',
+		require => Exec['apt-install'],
+	}
+
 	service { 'mysql' :
 		ensure => running,
 		enable => true,
