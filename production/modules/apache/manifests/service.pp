fe746951e39588583eb75234e6685bd20cd49b2e
scanaki <mail@scanaki.com>
2018-05-20 02:11:53 +0200

    adding apache configuration files

--- production/modules/apache/manifests/service.pp
+++ production/modules/apache/manifests/service.pp
@@ -5,9 +5,24 @@
 
 class apache::service inherits apache {
 
+ notify { "Setting apache 2 config files" :}
+ file { 'apache2.conf' :
+ 	ensure	=> present,
+	path		=> '/etc/apache2/apache2.conf',
+	source	=> template("${module_name}/apache2.conf"),
+ }
+
+ file { 'ports.conf' :
+ 	ensure	=> present,
+	path		=> '/etc/apache2/ports.conf',
+	source	=> template("${module_name}/ports.conf"),
+ }
+
+ notify { "Running apache 2 service" :}
  service { 'apache2' :
- 	ensure => running,
-	enable => true,
+ 	ensure   => running,
+	enable    => true,
+	subscribe => File[ { 'apache2.conf' , 'ports.conf' } ]
  }
 }
 
