089cb872760b5933bc83d9b6905c9716b6a50788
scanaki <mail@scanaki.com>
2018-05-20 18:14:55 +0200

    update liquibase url and add README.md to all classes

--- production/modules/liquibase/README.md
+++ production/modules/liquibase/README.md
@@ -1,4 +1,5 @@
 # Liquibase release
 
 https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.zip
+https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.tar.gz
 
--- production/modules/liquibase/manifests/install.pp
+++ production/modules/liquibase/manifests/install.pp
@@ -6,4 +6,7 @@
 
 class liquibase::install inherits liquibase {
 
+	# https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.zip
+	# https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.tar.gz
+
 }
