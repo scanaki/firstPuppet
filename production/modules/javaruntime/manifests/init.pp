13f0112426438c800b0ded8855cfb0f9dec3980a
scanaki <mail@scanaki.com>
2018-05-21 11:10:02 +0200

    Liquibase trying with jre7

--- production/modules/javaruntime/manifests/init.pp
+++ production/modules/javaruntime/manifests/init.pp
@@ -4,12 +4,18 @@
 # @desc:		Install Java runtime
 
 class javaruntime {
-	notify { "Installing Java runtime environment v.8" :}
-	package { 'openjdk-8-jre' :
+	notify { "Installing Java runtime environment v.7" :}
+	package { 'openjdk-7-jre' :
 		ensure	 => installed,
 		require	 => Exec['apt-install'],
 	}
 
+# 	notify { "Installing Java runtime environment v.8" :}
+# 	package { 'openjdk-8-jre' :
+# 		ensure	 => installed,
+# 		require	 => Exec['apt-install'],
+# 	}
+# 
 # 	notify { "Installing Java runtime environment v.9" :}
 # 	package { 'openjdk-9-jre' :
 # 		ensure	 => installed,
