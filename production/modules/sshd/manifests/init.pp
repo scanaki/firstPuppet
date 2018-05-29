9e1aa26c6a97fe3396a759e3008cd9a8af2d4972
scanaki <mail@scanaki.com>
2018-05-29 21:39:06 +0200

    remove trailing characters

--- production/modules/sshd/manifests/init.pp
+++ production/modules/sshd/manifests/init.pp
@@ -1,24 +1,24 @@
 # @file: sshd/manifests/init.pp
-# @author:	Thierry JEAN-LOUIS
+# @author: Thierry JEAN-LOUIS
 # @date: 20180517
 # @desc: ssh deamon
 
 class sshd {
-	
-	package { 'openssh-server' : 
-		ensure	=> present,
-	} ->
+ 
+ package { 'openssh-server' : 
+  ensure => present,
+ } ->
 
-##	Ci-dessous une ecriture identique
+## Ci-dessous une ecriture identique
 
-	file { '/etc/ssh/sshd_config' :
-		ensure	  => file,
-		mode		  => '0600',
-		source	  => 'puppet:///modules/sshd/sshd_config',
-	} ~>
+ file { '/etc/ssh/sshd_config' :
+  ensure   => file,
+  mode    => '0600',
+  source   => 'puppet:///modules/sshd/sshd_config',
+ } ~>
 
-	service { 'sshd' :
-		ensure 	    => running,
-		enable	    => true,
-	}
+ service { 'sshd' :
+  ensure      => running,
+  enable     => true,
+ }
 }
